f416271ffd1663425bf6dd7992ceb6aea95edfc3
Henry Doan <henrydoan96@gmail.com>
2019-08-13 10:27:49 -0600

    notes backend

---
+++ app/controllers/api/notes_controller.rb
@@ -0,0 +1,34 @@
+class Api::NotesController < ApplicationController
+  def index
+    render json: @current_user.notes.all
+  end
+
+  def create
+    note = @current_user.notes.new(note_params)
+    if note.save
+      render json: note
+    else
+      render json: { errors: note.errors }, status: :unprocessable_entity 
+    end
+  end
+
+  def update
+    note = @current_user.notes.find(params[:id])
+    if note.update(note_params)
+      render json: note
+    else
+      render json: { errors: note.errors }, status: :unprocessable_entity 
+    end
+  end
+
+  def destroy
+    @current_user.notes.find(params[:id]).destroy
+    render json: { message: 'Note deleted' }
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
+end
