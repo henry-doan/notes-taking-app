708242cd1003f0e87a1dbef4caa42bdf8bb025ec
Henry Doan <henrydoan96@gmail.com>
2019-08-14 11:14:56 -0600

    add front end starter

---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+const Home = ({}) => (
+  <>
+    <Header>Home</Header>
+  </>
+)
+
+export default Home;
---
+++ client/src/components/shared/NoMatch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+const NoMatch = ({}) => (
+  <>
+    <Header>Page Not Found</Header>
+  </>
+)
+
+export default NoMatch;
