eb0979598f20f50d3d5e5d33dd5338ca3ec6f0e5
Henry Doan <henrydoan96@gmail.com>
2019-08-20 07:58:56 -0600

    useform done

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -6,6 +6,43 @@ const useForm = (cb, validate, initial = {}) => {
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [submitAttempts, setSubmitAttempts] = useState(0)
   
+
+  useEffect(() => {
+    if (Object.keys(errs).length === 0 && isSubmitting) {
+      cb();
+    }
+    setIsSubmitting(false);
+  }, [errs])
+
+  useEffect(() => {
+    setIsSubmitting(false)
+    if (submitAttempts > 0){
+      setErrs(validate(values))
+    }
+  }, [values])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setSubmitAttempts(submitAttempts + 1)
+    setErrs(validate(values))
+    setIsSubmitting(true)
+  }
+
+  const handleChange = (name, value) => {
+    if (!isSubmitting) {
+      setValues(values => ({
+        ...values,
+        [name]: value
+      }))
+    }
+  }
+
+  return {
+    handleChange,
+    handleSubmit,
+    values,
+    errs
+  }
 }
 
 export default useForm;
